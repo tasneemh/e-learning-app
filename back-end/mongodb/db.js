6766a3e2a8cca5b8a4ee5367677d3f53846bd975
Tasneem Burmawala <tasneemhburmawala@gmail.com>
2021-03-06 10:21:26 -0500

    adding all files

--- back-end/mongodb/db.js
+++ back-end/mongodb/db.js
@@ -9,6 +9,12 @@ const dbName = 'myproject';
 const mongodbSetup = (callback) => {
   MongoClient.connect(url, function(err, client) {
     console.log("monogodb connected successfully to server");
+    // const db = client.db(dbName);
+    // const collection = db.collection('documents');
+    // // Insert some documents
+    // collection.insertMany([
+    // {a : 1}, {a : 2}, {a : 3}
+    // ]);
     callback(client.db(dbName));
   });
 }  
